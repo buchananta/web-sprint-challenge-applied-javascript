d366fcd3c713a8e6260db96f1998e82de258fc41
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-08-07 10:46:55 -0500

    added cards for articles

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,34 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+//keep in mind, the structure is {articles: {subjects: [actualArticlesArr]} }
+//that is SUPER annoying.
+function makeCard(article) {
+  const element = document.createElement('div');
+  element.className = 'card';
+  element.innerHTML = `<div class="headline">${article.headline}</div>
+   <div class="author">
+     <div class="img-container">
+       <img src=${article.authorPhoto} />
+     </div>
+     <span>By ${article.authorName}</span>
+   </div>`;
+   //now create a listener
+   element.addEventListener('click', event => {
+     console.log(element.querySelector('.headline').textContent);
+   })
+   //and finally return the whole thing!
+   return document.querySelector('.cards-container').appendChild(element);
+}
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+  .then(result => {
+    //this is going to be a mess
+    for (topic in result.data.articles) {
+      result.data.articles[topic].forEach(article => {
+        makeCard(article);
+      })
+    }
+  })
